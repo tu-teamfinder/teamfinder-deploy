5593e8f01075062a2e6bf5c28534394744d95c78
Tanapat Sa-nguantud <6510615120@cn330>
2024-12-14 22:26:48 +0700

    deploy 1.7

--- teamfinder/teamfinder/asgi.py
+++ teamfinder/teamfinder/asgi.py
@@ -4,16 +4,10 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
-from whitenoise import WhiteNoise
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teamfinder.settings')
 
 django_asgi_app = get_asgi_application()
-
-django_asgi_app = WhiteNoise(django_asgi_app, root=os.path.join(BASE_DIR, 'staticfiles'))
 
 from chat import routing
 
