a91c0c4f1e05c76a3fa5047edaf2435192fc5d09
Tanapat Sa-nguantud <6510615120@cn330>
2024-12-14 19:06:10 +0700

    asgi.py

--- teamfinder/teamfinder/asgi.py
+++ teamfinder/teamfinder/asgi.py
@@ -1,24 +1,19 @@
 import os
-import sys
-import django
-
-project_home = '/home/teamfinder'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'teamfinder.settings'
-# django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
+
+django_asgi_app = get_asgi_application()
+
 from chat import routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket":AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
-        )
-    )
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+    ),
 })
