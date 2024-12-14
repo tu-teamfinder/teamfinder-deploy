69e3df5470c10c53eaff26f14fb412d2b38f4ba0
Tanapat Sa-nguantud <6510615120@cn330>
2024-12-14 22:06:36 +0700

    deploy 1.5

--- teamfinder/teamfinder/settings.py
+++ teamfinder/teamfinder/settings.py
@@ -165,18 +165,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 if ENVIRONMENT == 'development':
     MEDIA_ROOT = BASE_DIR / 'media'
